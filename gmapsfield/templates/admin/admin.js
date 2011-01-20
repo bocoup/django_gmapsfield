1d4943e1c983e8343ecf70563ded661a4b8db697
root <root@thinkpad.localdomain>
2011-01-20 11:29:34 -0500

    Updated to fix jQuery detection in admin script

--- gmapsfield/templates/admin/admin.js
+++ gmapsfield/templates/admin/admin.js
@@ -1,79 +1,83 @@
-django.jQuery(function($) {
-    
-    // Construct useful closure per map instance
-    $(".google-map[type=text]").live("initialize-map", function(evt) {
-        var map, data,
-            orig = $(this),
-            clone = orig.clone(true).attr("type", "hidden"),
-            defaults = {
-                size: ["500px", "350px"],
-                coordinates: [-34.397, 150.644],
-                zoom: 8
-            };
+~function(window, document) {
+    var jQ = jQuery || (django && django.jQuery);
 
-        // Replace map with clone
-        orig.replaceWith(clone);
-
-        // Set new map div
-        map = $("<div class='map'/>");
-
-        // Place after clone
-        clone.after(map);
-
-        // Parse data
-        try {
-            data = $.parseJSON(orig.val());
-            data = $.extend({}, defaults, data);
-        } catch(ex) {
-            data = defaults;
-        }
-
-        // Set width/height
-        map.css({ width: data.size[0], height: data.size[1] });
-
-        // Assign coordinates
-        var coordinates = new google.maps.LatLng(data.coordinates[0], data.coordinates[1]),
-            // Init the map
-            gmap = new google.maps.Map(map[0], {
-                zoom: data.zoom,
-                center: coordinates,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            });
+    jQ(function($) {
         
-        // Add marker used for center positioning the map
-        var center = new google.maps.Marker({
-            position: coordinates,
-            map: gmap,
-            draggable: true,
-            title: "Centered Here"
+        // Construct useful closure per map instance
+        $(".google-map[type=text]").live("initialize-map", function(evt) {
+            var map, data,
+                orig = $(this),
+                clone = orig.clone(true).attr("type", "hidden"),
+                defaults = {
+                    size: ["500px", "350px"],
+                    coordinates: [-34.397, 150.644],
+                    zoom: 8
+                };
+
+            // Replace map with clone
+            orig.replaceWith(clone);
+
+            // Set new map div
+            map = $("<div class='map'/>");
+
+            // Place after clone
+            clone.after(map);
+
+            // Parse data
+            try {
+                data = $.parseJSON(orig.val());
+                data = $.extend({}, defaults, data);
+            } catch(ex) {
+                data = defaults;
+            }
+
+            // Set width/height
+            map.css({ width: data.size[0], height: data.size[1] });
+
+            // Assign coordinates
+            var coordinates = new google.maps.LatLng(data.coordinates[0], data.coordinates[1]),
+                // Init the map
+                gmap = new google.maps.Map(map[0], {
+                    zoom: data.zoom,
+                    center: coordinates,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
+            
+            // Add marker used for center positioning the map
+            var center = new google.maps.Marker({
+                position: coordinates,
+                map: gmap,
+                draggable: true,
+                title: "Centered Here"
+            });
+
+            // Update function
+            function update(evt) {
+                gmap.setCenter(center.position);
+                data.coordinates = [ center.position.lat(), center.position.lng() ];
+                data.zoom = gmap.zoom;
+                clone.val( JSON.stringify(data) );
+            }
+
+            // Center map on marker drop
+            google.maps.event.addListener(center, "mouseup", update);
+            // When zoom changed
+            google.maps.event.addListener(gmap, "zoom_changed", update);
         });
 
-        // Update function
-        function update(evt) {
-            gmap.setCenter(center.position);
-            data.coordinates = [ center.position.lat(), center.position.lng() ];
-            data.zoom = gmap.zoom;
-            clone.val( JSON.stringify(data) );
-        }
+        // Reference to each map and reference clone
+        var map = $(".google-map[type=text]").each(function(evt) {
+            $(this).trigger("initialize-map");
+        });
 
-        // Center map on marker drop
-        google.maps.event.addListener(center, "mouseup", update);
-        // When zoom changed
-        google.maps.event.addListener(gmap, "zoom_changed", update);
+        $(".add-map").bind("click", function() {
+            var that = $(this),
+                widget = $("<input class='google-map' type='text' name='"+ that.attr("data-name") +"' value='"+ that.attr("data-value") +"' type='text' />");
+
+            that.replaceWith(widget);
+            widget.trigger("initialize-map");
+            return false;
+        });
+
     });
-
-    // Reference to each map and reference clone
-    var map = $(".google-map[type=text]").each(function(evt) {
-        $(this).trigger("initialize-map");
-    });
-
-    $(".add-map").bind("click", function() {
-        var that = $(this),
-            widget = $("<input class='google-map' type='text' name='"+ that.attr("data-name") +"' value='"+ that.attr("data-value") +"' type='text' />");
-
-        that.replaceWith(widget);
-        widget.trigger("initialize-map");
-        return false;
-    });
-
-});
+}(this, this.document);
