cccde21323916c605d36e53c262086c820a9f3ea
Nahuel Defossé <nahuel.defosse@gmail.com>
2012-02-13 18:38:07 -0300

    Act

--- gmapsfield/forms.py
+++ gmapsfield/forms.py
@@ -1,17 +1,23 @@
 from django.forms import widgets
 from django.template import Context, loader
-from django.conf import settings
+from conf import settings
+
+#===============================================================================
+# Avoid jQuery to be loaded twice if it's there 
+#===============================================================================
+
+
 
 class GoogleMapsFormWidget(widgets.Widget):
 
     class Media:
         js = ('http://maps.google.com/maps/api/js?sensor=false', 
-              'http://code.jquery.com/jquery.min.js', 
-              'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js', 
+              settings.GMAP_JQUERY, 
+              settings.GMAP_JQUERY_UI, 
               settings.STATIC_URL + 'admin/gmapsfield/admin.js',)
 
         css = {
-            'all': ('http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/themes/smoothness/jquery-ui.css',),
+            'all': (settings.GMAP_JQUERY_UI_CSS,),
         }
 
     def __init__(self, *args, **kwargs):
